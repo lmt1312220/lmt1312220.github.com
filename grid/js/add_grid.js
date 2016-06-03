2a5281f112cd8fa64b8d0c8cf0436c7c295e0b7f
lmt1312220 <774217991@qq.com>
2016-06-03 15:49:53 +0800

    修改了grid文件夹“

--- grid/js/add_grid.js
+++ grid/js/add_grid.js
@@ -1,19 +1,43 @@
-var m;
+
+var jingguoyue;
 //today
 //过推荐的日期了一段时间后
-function show1(m){
-　　var mydate = new Date();
-// var str;
-if(parseInt(mydate.getMonth()+1+m)>12)
-　　 str= "" + (mydate.getFullYear()+1) + "-";
-else
- str= "" + mydate.getFullYear() + "-";
-　　str += (mydate.getMonth()+1+m) + "-";
-　　str += mydate.getDate() ;
-　　return str;
-　　}
+function getCTime(m) { 
+          var d = new Date(); 
+          var year = d.getFullYear()+parseInt((parseInt(m)/12)); 
+          var month = d.getMonth() + 1+(parseInt(m)%12); 
+          var date = d.getDate(); 
+          var week = d.getDay(); 
+          var curDateTime = year; 
+          if (month > 9) 
+          curDateTime = curDateTime + "-" + month; 
+          else
+          curDateTime = curDateTime + "-0" + month; 
+          if (date > 9) 
+          curDateTime = curDateTime + "-" + date; 
+          else
+          curDateTime = curDateTime + "-0" + date; 
+          return curDateTime;
+}
+function getCurrentDateTime() { 
+          var d = new Date(); 
+          var year = d.getFullYear(); 
+          var month = d.getMonth() + 1; 
+          var date = d.getDate(); 
+          var week = d.getDay(); 
+          var curDateTime = year; 
+          if (month > 9) 
+          curDateTime = curDateTime + "-" + month; 
+          else
+          curDateTime = curDateTime + "-0" + month; 
+          if (date > 9) 
+          curDateTime = curDateTime + "-" + date ; 
+          else
+          curDateTime = curDateTime + "-0" + date;
+          return curDateTime;
+}
 //计算俩个时间的时间差的函数
-   function  DateDiff(sDate1,  sDate2){    //sDate1和sDate2是2002-12-18格式  
+function  DateDiff(sDate1,  sDate2){    //sDate1和sDate2是2002-12-18格式  
        var  aDate,  oDate1,  oDate2,  iDays  
        aDate  =  sDate1.split("-")  
        oDate1  =  new  Date(aDate[1]  +  '-'  +  aDate[2]  +  '-'  +  aDate[0])    //转换为12-18-2002格式  
@@ -22,62 +46,41 @@ else
        iDays  =  parseInt(Math.abs(oDate1  -  oDate2)  /  1000  /  60  /  60  /24)    //把相差的毫秒数转换为天数  
        return  iDays  
 }
+function jisuanriqijiange(){
+   var val = $(".p2").val();
+   var valTime = $(".p3").val(); 
+   var time_jiange=DateDiff(getCurrentDateTime(),valTime);
+   var  a=parseInt(parseInt(val)/time_jiange);
+   $(".p4").html(time_jiange+"天<br/>利率为0.06%<br/>推荐每天存"+a+"元");
+}
 
-   $(document).ready(function(){
-   	 $(".introduce_time").click(function(){  
- var val = $(".p2").val();
- if(parseInt(val)<=1000){
- 	var  a=parseInt(parseInt(val)/60);
- 	$("div.modal-body ").html("推荐存2个月，利率为0.02%，每日大约存"+a+"元，是否接受这种推荐");
-    	// $(".p4").html("推荐存2个月<br/>利率为0.02%<br/>每日大约存"+a+"元")
-    	m=2; 
- }
-    else if(parseInt(val)>=1000||parseInt(val)<=3000){
-    	var  a=parseInt(parseInt(val)/120);
-    	$("div.modal-body").html("推荐存4个月，利率为0.04%，每日大约存"+a+"元，是否接受这种推荐");
-    	$(".p4").html("推荐存4个月<br/>利率为0.04%<br/>每日大约存"+a+"元")
-    	m=4; 
-    	} 
 
-    	else if(parseInt(val)>=3000||parseInt(val)<=5000){
-    		var  a=parseInt(parseInt(val)/180);
-$("div.modal-body").html("推荐存6个月，利率为0.06%，每日大约存"+a+"元，是否接受这种推荐");
-    	m=6; 
-    	}
-    	else{
-    		$("div.modal-body").html("推荐存8个月，利率为0.08%，每日大约存"+a+"元，是否接受这种推荐");
-    	m=8; 
-    	}
+$(init);
+
+
+function init () {
+  $(".introduce_time").click(function(){  
+          var val = $(".p2").val();
+          if(parseInt(val)<=1000)
+              jingguoyue=2;
+          var  a=parseInt(parseInt(val)/(30*jingguoyue)); 
+          $("div.modal-body ").html("推荐存"+jingguoyue+"个月，利率为0.02%，每日大约存"+a+"元，是否接受这种推荐");
   });
-     $("button#yes").click(function(){
-   $(".p3").val(show1(m));
-});
+$('#demo1').attr("readonly","readonly");
+  $("button#yes").click(function(){
+        $(".p3").val(getCTime(jingguoyue));
+        var val = $(".p2").val();
+        var valTime = $(".p3").val(); 
+        var time_jiange=DateDiff(getCurrentDateTime(),valTime);
+        var  a=parseInt(parseInt(val)/(time_jiange)); 
+           $(".p4").html(time_jiange+"天<br/>利率为0.06%<br/>推荐每天存"+a+"元");
+  });
+  $("button#no").click(function(){
+        $(".p3").val(""); 
+        $(".p4").html("");
+  });
 
-  $("#demo1").keyup(function(){
-var mydate1 = new Date();
-　　var str = "" + mydate.getFullYear() + "-";
-　　str += (mydate.getMonth()+1) + "-";
-　　str += mydate.getDate();
-var val = $(".p3").val();
- var time_jiange=DateDiff(str,val);
-
- $(".p4").html("推荐存"+time_jiange+"个月，利率为0.06%，每日大约存"+a+"元，是否接受这种推荐");
-});
-  //计算俩个时间的时间差
-//  $("#demo1").change(function(){  
-//    $(this).css("background-color","#FFFFCC");
-//     var mydate1 = new Date();
-// 　　var str = "" + mydate.getFullYear() + "-";
-// 　　str += (mydate.getMonth()+1) + "-";
-// 　　str += mydate.getDate();
-// var val = $(".p3").val();
-//  var time_jiange=DateDiff(str,val);
-
-//  $(".p4").html("推荐存"+time_jiange+"个月，利率为0.06%，每日大约存"+a+"元，是否接受这种推荐");
-// });
-
-});
-
+}
 
 
 //在按yes按钮时推荐日期
